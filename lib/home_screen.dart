78d184d57c45924db431dae9200b481690a05aef
unknown <mitjho@gmail.com>
2023-11-22 14:32:44 +0200

    second commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:alphapick/main.dart';
+
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            Text('Team A', style: TextStyle(fontSize: 20),),
+            Text('v', style: TextStyle(fontSize: 20),),
+            Text('Team B', style: TextStyle(fontSize: 20),),
+          ],
+        ),
+      ),
+    );
+  }
+}
